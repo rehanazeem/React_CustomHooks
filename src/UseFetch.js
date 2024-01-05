305a794034e3863f0608355fdd94e842318f65f0
Rehan <rehanzzz1590@gmail.com>
2024-01-05 10:31:19 +0500

    Done

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -18,7 +18,7 @@ export function useFetch(fetchFn, initialValue) {
           setIsFetching(false);
         }
     
-        fetchedData();
+        fetchData();
       }, [fetchFn]);
 
       return {
